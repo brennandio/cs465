c000ab314335481709a95985ede3a931ace9dff8
brennandio <131428784+brennandio@users.noreply.github.com>
2025-08-18 02:43:14 -0700

    Module 7 completed baseline

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -4,6 +4,7 @@ import { TripCardComponent } from '../trip-card/trip-card.component';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -22,7 +23,8 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    private router: Router
+    private router: Router, 
+    private authenticationService: AuthenticationService
   ) {
     console.log('trip-listing constructor');
   }
@@ -30,6 +32,11 @@ export class TripListingComponent implements OnInit {
   public addTrip(): void {
     this.router.navigate(['add-trip']);
   }
+
+  public isLoggedIn() 
+  { 
+    return this.authenticationService.isLoggedIn(); 
+  } 
 
   private getStuff(): void {
     this.tripDataService.getTrips()
